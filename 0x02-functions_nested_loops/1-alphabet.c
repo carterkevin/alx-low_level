6d26f1a7a1619dbf6a365990f5115d22cfeb9f35
carterkevin <nkatakelvin1@gmail.com>
2022-10-18 17:42:55 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "maim.h"
+
+/**
+ * print_alphabet - print the alpha in lowercase
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
