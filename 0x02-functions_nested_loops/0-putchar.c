20b41c2fcefa855b541827dec94a7b29a59f6e71
carterkevin <nkatakelvin1@gmail.com>
2022-10-18 17:04:26 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main -prints _putchar and a new line
+ * Return: Always
+ */
+int main(void)
+{
+	-putchr('_');
+	-putchar('p');
+	-putchar('u');
+	-putchar('t');
+	-putchar('c');
+	-putchar('h');
+	-putchar('a');
+	-putchar('r');
+	-putchar('\n');
+
+	return (0);
