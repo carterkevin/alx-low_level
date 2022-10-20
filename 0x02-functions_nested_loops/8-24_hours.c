c27d0d2de75c559b19f64dd155f5e02a7622ca44
carterkevin <nkatakelvin1@gmail.com>
2022-10-19 17:16:23 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,8 +2,9 @@
 
 /**
  * jacj_bauer - prints every minute of the day
+ *
  */
-main(void)
+void jack_bauer(void)
 {
 	int hour, minute;
 
@@ -12,12 +13,12 @@ main(void)
 		for (minute = 0; minute <= 59; minute++)
 
 		{
-			-putchar((hour / 10) + '0');
-			-putchar((hour % 10) + '0');
-			-putchar(':');
-			-putchar((minute / 10) + '0');
-			-putchar((minute % 10) + '0');
-			-putchar('\n');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
