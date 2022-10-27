dd342c44a176e18dc1c293b648efbb1717ab40f1
carterkevin <nkatakelvin1@gmail.com>
2022-10-26 17:38:16 -0700

    Strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,22 +8,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int k;
+	int i;
 	int j;
 
 	k = 0;
-	while (dest[k] != '\0')
+	while (dest[i] != '\0')
 	{
-		k++;
+		i++;
 	}
 	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[k] = src[k];
-		k++;
+		i++;
 		j++;
 
 	}
-	dest[k] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
